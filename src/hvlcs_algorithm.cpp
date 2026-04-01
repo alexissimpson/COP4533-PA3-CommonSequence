e11c1c8b9e86695444139e26092ef1ca9cfb6e41
lesim <lesimpson30@gmail.com>
2026-04-01 16:45:51 -0400

    Added algorithm based on recurrence eq

---
+++ src/hvlcs_algorithm.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include "letters.h"
+using namespace std;
+
+void computeHVLCS(string A, string B, vector<Letter> alphabet, ofstream& outfile){
+    // initialize 2d vector M
+    vector<vector<int>> M(A.length()+1, vector<int> (B.length()+1, 0));
+
+    // base case: i=0 or j=0
+    for(int i = 0; i < A.length(); i++){
+        M[i][0] = 0;
+    }
+    for(int j = 0; j < B.length(); j++){
+        M[0][j] = 0;
+    }
+
+    // recursive case
+    for(int i = 1; i < A.length()+1; i++){
+        for(int j = 1; j < B.length()+1; j++){
+            int value_i;
+            for(int k = 0; k < alphabet.size(); k++){
+                if(alphabet[k].getCharacter() == A[i]){
+                    value_i = alphabet[k].getValue();
+                }
+            }
+            M[i][j] = max({value_i + M[i-1][j-1], M[i-1][j], M[i][j-1]});
+        }
+    }
+
+    // print max value of common subsequence
+    outfile << M[A.length()][B.length()] << endl;
+}
---
+++ src/hvlcs_algorithm.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include<vector>
+#include<fstream>
+using namespace std;
+
+void computeHVLCS(string A, string B, vector<Letter> alphabet, ofstream& outfile);
